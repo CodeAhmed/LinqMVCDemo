96ff2f7def86d2bd2c5b122217acb50a230c29cb
CodeAhmed <mansooronly4u@gmail.com>
2016-06-23 02:13:49 +0530

    Using Hardcoded List to diplay Menu as Json Data

--- LinqMVCDemo/Controllers/HomeController.cs
+++ LinqMVCDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinqMVCDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,38 @@ namespace LinqMVCDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //
+        // GET: /Home/
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public JsonResult Index()
         {
-            return View();
+            List<Menu> demos = new List<Menu>(){
+                new Menu(){ObjType = "OT1", ObjSubType = "OST1", ObjName= "ON1"},
+                new Menu(){ObjType = "OT1", ObjSubType = "OST1.1", ObjName= "ON1.1"},
+                new Menu(){ObjType = "OT1", ObjSubType = "OST1.2", ObjName= "ON1.2"},
+                new Menu(){ObjType = "OT2", ObjSubType = "OST2", ObjName= "ON2"},
+                new Menu(){ObjType = "OT2", ObjSubType = "OST2.1", ObjName= "ON2.1"},
+                new Menu(){ObjType = "OT2", ObjSubType = "OST2.2", ObjName= "ON2.3"},
+                new Menu(){ObjType = "OT3", ObjSubType = "OST3", ObjName= "ON3"},
+                new Menu(){ObjType = "OT3", ObjSubType = "OST3.1", ObjName= "ON3.1"},
+                new Menu(){ObjType = "OT3", ObjSubType = "OST3.2", ObjName= "ON3.2"},
+                new Menu(){ObjType = "OT3", ObjSubType = "OST3.3", ObjName= "ON3.3"},
+            };
+            List<Menu> children = new List<Menu>();
+            var query = from temp in demos
+                        group temp by new { temp.ObjType, temp.ObjSubType,temp.ObjName } into newgrp
+                        select new Menu()
+                        {
+                            ObjType= newgrp.Key.ObjType,
+                            ObjSubType = newgrp.Key.ObjSubType,
+                            ObjName = newgrp.Key.ObjName,
+                            children = newgrp.ToList(),
+                        };
+            return Json(query, JsonRequestBehavior.AllowGet);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
+	}
 }
---
+++ LinqMVCDemo/Models/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinqMVCDemo.Models
+{
+    public class Menu
+    {
+        public string ObjType { get; set; }
+        public string ObjSubType { get; set; }
+        public string ObjName { get; set; }
+        public List<Menu> children { get; set; }
+    }
+}
