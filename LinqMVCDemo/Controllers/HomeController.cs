69cc472b275d1a06e330303371a45a80ba4d32a9
CodeAhmed <mansooronly4u@gmail.com>
2016-06-27 01:16:27 +0530

    Menu List Got fixed with IGrouping function in View

--- LinqMVCDemo/Controllers/HomeController.cs
+++ LinqMVCDemo/Controllers/HomeController.cs
@@ -11,121 +11,30 @@ namespace LinqMVCDemo.Controllers
     {
         //
         // GET: /Home/
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
 
-        //public JsonResult Index()
         public ActionResult Index()
         {
             List<Menu> demos = new List<Menu>(){
                 new Menu(){ObjType = "OT1", ObjSubType = "OST1", ObjName= "ON1"},
+                new Menu(){ObjType = "OT1", ObjSubType = "OST1", ObjName= "ON2"},
                 new Menu(){ObjType = "OT1", ObjSubType = "OST1.1", ObjName= "ON1.1"},
-                //new Menu(){ObjType = "OT1", ObjSubType = "OST1.2", ObjName= "ON1.2"},
+                new Menu(){ObjType = "OT1", ObjSubType = "OST1.2", ObjName= "ON1.2"},
                 new Menu(){ObjType = "OT2", ObjSubType = "OST2", ObjName= "ON2"},
                 new Menu(){ObjType = "OT2", ObjSubType = "OST2.1", ObjName= "ON2.1"},
-                //new Menu(){ObjType = "OT2", ObjSubType = "OST2.2", ObjName= "ON2.3"},
+                new Menu(){ObjType = "OT2", ObjSubType = "OST2.2", ObjName= "ON2.2"},
                 new Menu(){ObjType = "OT3", ObjSubType = "OST3", ObjName= "ON3"},
+                new Menu(){ObjType = "OT3", ObjSubType = "OST3", ObjName= "ON4"},
                 new Menu(){ObjType = "OT3", ObjSubType = "OST3.1", ObjName= "ON3.1"},
-                //new Menu(){ObjType = "OT3", ObjSubType = "OST3.2", ObjName= "ON3.2"},
-                //new Menu(){ObjType = "OT3", ObjSubType = "OST3.3", ObjName= "ON3.3"},
+                new Menu(){ObjType = "OT3", ObjSubType = "OST3.2", ObjName= "ON3.2"},
             };
-            List<ObjectData> obj = new List<ObjectData>();
+
             var query = demos.GroupBy(m => m.ObjType).ToList();
-            foreach (var subgroup in query)
+            foreach (var item in query)
             {
-                obj.Add(new ObjectData() { ObjectName = subgroup.Key, ObjectType = "Type" });
-                //Console.WriteLine(subgroup.Key + ":");
-                var query2 = subgroup.GroupBy(m => m.ObjSubType);
-                foreach (var subgroup2 in query2)
-                {
-                    obj.Add(new ObjectData() { ObjectName = subgroup2.Key, ObjectType = "SubType" });
-                    //Console.WriteLine(subgropu2.Key);
-                    foreach (Menu m in subgroup2)
-                        obj.Add(new ObjectData() { ObjectName = m.ObjName, ObjectType = "Object" });
-                    //Console.WriteLine(m.ObjName);
-                }
-
-
+                var result = item.GroupBy(m => m.ObjSubType).ToList();
             }
-            // IList<MenuData> obj = new List<MenuData>();
-            // var query = demos.GroupBy(m => m.ObjType).ToList();
-            // foreach (var subgroup in query)
-            // {
+            return View(query);
 
-            //     obj.Add(new MenuData() { ObjType = subgroup.Key });
-            //     //Console.WriteLine(subgroup.Key + ":");
-            //     var query2 = subgroup.GroupBy(m => m.ObjSubType);
-            //     foreach (var subgroup2 in query2)
-            //     {
-            //         obj.Add(new MenuData() { ObjSubType = subgroup2.Key });
-            //         //Console.WriteLine(subgropu2.Key);
-            //         foreach (Menu m in subgroup2)
-            //             obj.Add(new MenuData() { ObjName = m.ObjName });
-            //         //Console.WriteLine(m.ObjName);
-            //     }
-
-
-            // }
-            //// return Json(obj, JsonRequestBehavior.AllowGet);
-            return View(obj);
-        }
-
-
-            public ActionResult View1()
-        {
-            List<Menu> demos = new List<Menu>(){
-                new Menu(){ObjType = "OT1", ObjSubType = "OST1", ObjName= "ON1"},
-                new Menu(){ObjType = "OT1", ObjSubType = "OST1.1", ObjName= "ON1.1"},
-                //new Menu(){ObjType = "OT1", ObjSubType = "OST1.2", ObjName= "ON1.2"},
-                new Menu(){ObjType = "OT2", ObjSubType = "OST2", ObjName= "ON2"},
-                new Menu(){ObjType = "OT2", ObjSubType = "OST2.1", ObjName= "ON2.1"},
-                //new Menu(){ObjType = "OT2", ObjSubType = "OST2.2", ObjName= "ON2.3"},
-                new Menu(){ObjType = "OT3", ObjSubType = "OST3", ObjName= "ON3"},
-                new Menu(){ObjType = "OT3", ObjSubType = "OST3.1", ObjName= "ON3.1"},
-                //new Menu(){ObjType = "OT3", ObjSubType = "OST3.2", ObjName= "ON3.2"},
-                //new Menu(){ObjType = "OT3", ObjSubType = "OST3.3", ObjName= "ON3.3"},
-            };
-            List<ObjectData> obj = new List<ObjectData>();
-            var query = demos.GroupBy(m => m.ObjType).ToList();
-            foreach (var subgroup in query)
-            {
-                obj.Add(new ObjectData() { ObjectName = subgroup.Key, ObjectType = "Type" });
-                //Console.WriteLine(subgroup.Key + ":");
-                var query2 = subgroup.GroupBy(m => m.ObjSubType);
-                foreach (var subgroup2 in query2)
-                {
-                    obj.Add(new ObjectData() { ObjectName = subgroup2.Key, ObjectType = "SubType" });
-                    //Console.WriteLine(subgropu2.Key);
-                    foreach (Menu m in subgroup2)
-                        obj.Add(new ObjectData() { ObjectName = m.ObjName, ObjectType = "Object" });
-                    //Console.WriteLine(m.ObjName);
-                }
-
-
-            }
-           // IList<MenuData> obj = new List<MenuData>();
-           // var query = demos.GroupBy(m => m.ObjType).ToList();
-           // foreach (var subgroup in query)
-           // {
-                
-           //     obj.Add(new MenuData() { ObjType = subgroup.Key });
-           //     //Console.WriteLine(subgroup.Key + ":");
-           //     var query2 = subgroup.GroupBy(m => m.ObjSubType);
-           //     foreach (var subgroup2 in query2)
-           //     {
-           //         obj.Add(new MenuData() { ObjSubType = subgroup2.Key });
-           //         //Console.WriteLine(subgropu2.Key);
-           //         foreach (Menu m in subgroup2)
-           //             obj.Add(new MenuData() { ObjName = m.ObjName });
-           //         //Console.WriteLine(m.ObjName);
-           //     }
-
-
-           // }
-           //// return Json(obj, JsonRequestBehavior.AllowGet);
-            return View(obj);
         }
     }
 }
--- LinqMVCDemo/Models/Menu.cs
+++ LinqMVCDemo/Models/Menu.cs
@@ -12,18 +12,4 @@ namespace LinqMVCDemo.Models
         public string ObjName { get; set; }
         public List<Menu> children { get; set; }
     }
-
-    public class ObjectData
-    {
-        public string ObjectName { get; set; }
-        public string ObjectType { get; set; }
-    }
-
-    public class MenuData
-    {
-        public string ObjType { get; set; }
-        public string ObjSubType { get; set; }
-        public string ObjName { get; set; }
-        //public List<MenuData> list { get; set; }
-    }
 }
